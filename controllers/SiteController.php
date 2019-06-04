88078b37fb57ac1daf87af93fec2466ca137bd4a
川北 紘正 <kawakita@mac185.local>
2019-06-04 13:51:32 +0900

    create todos files of models and views

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -64,6 +64,11 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
+    public function actionTodo()
+    {
+        return $this->render("todo");
+    }
+
     /**
      * Login action.
      *
---
+++ models/Todos.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "todos".
+ *
+ * @property int $id
+ * @property string $task
+ * @property int $done
+ */
+class Todos extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'todos';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['task'], 'required'],
+            [['done'], 'integer'],
+            [['task'], 'string', 'max' => 256],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'task' => 'Task',
+            'done' => 'Done',
+        ];
+    }
+}
