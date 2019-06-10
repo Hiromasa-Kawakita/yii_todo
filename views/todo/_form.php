e576e2be6d42a1d41bc2b7a4687f30538256d90f
川北 紘正 <kawakita@mac185.local>
2019-06-10 11:28:46 +0900

    update task form hint and remove done form.

--- views/todo/_form.php
+++ views/todo/_form.php
@@ -12,9 +12,9 @@ use yii\widgets\ActiveForm;
 
 <?php $form = ActiveForm::begin(); ?>
 
-<?= $form->field($model, 'task')->textInput(['maxlength' => true])->hint("タスクを入力してください。")->label("Task") ?>
+<?= $form->field($model, 'task')->textInput(['maxlength' => true])->hint("タスクを入力してください。(最大255文字)")->label("Task") ?>
 
-<?= $form->field($model, 'done')->textInput()->hint("達成：1, 未達成：0を入力してください")->label("Done") ?>
+<!-- <?= $form->field($model, 'done')->textInput()->hint("達成：1, 未達成：0を入力してください")->label("Done") ?> -->
 
 <div class="form-group">
     <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
