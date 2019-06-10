49aeb80259de6c7d19621be2f60bb119c025749f
川北 紘正 <kawakita@mac185.local>
2019-06-10 11:30:32 +0900

    update task list.(checkbox, update, detail, delete)

--- views/todo/index.php
+++ views/todo/index.php
@@ -11,15 +11,60 @@ $this->title = 'Todo list';
 $this->params['breadcrumbs'][] = $this->title;
 // var_dump($todos);
 ?>
-<h1>todoリスト</h1>
+<h1>Todoリスト</h1>
 
 <p>
     <?= Html::a('Create Task', ['create'], ['class' => 'btn btn-success']) ?>
 </p>
 
-<?php
+--------------------------------------------------------------------------------------------------------
+<?php 
 foreach ($todos as $todo): ?>
     <li>
-        <?= Html::encode("{$todo->id}, {$todo->task}, {$todo->done}") ?>
+    <?= Html::beginForm(['done', 'id' => $todo->id], 'post') ?>
+        <?php  if($todo->done == 0):?>
+            <input type="checkbox" name="done" <?= $todo->done ? 'checked': ''; ?>>
+            <input type="submit", value = "タスク更新">
+        <?php else: ?>
+            <?= Html::encode("[完了]") ?>
+        <?php endif; ?>
+        <?= Html::endForm() ?>
+        
+        <?= Html::encode("タスク：{$todo->task}") ?>
+
+        <!-- <?= Html::encode("タスク：{$todo->task}, done：{$todo->done}") ?> -->
+        <!-- 
+        <?= Html::beginForm(['todo/delete', 'id' => $todo->id], 'post') ?>
+        <?= Html::submitButton('Delete Task', ['class' => 'btn btn-danger btn-xs']) ?>
+        <?= Html::endForm() ?> 
+        -->
+        <p>
+            <?= Html::a('Update', ['update', 'id' => $todo->id], ['class' => 'btn btn-primary btn-xs']) ?>
+            <?= Html::a('Detail', ['view', 'id' => $todo->id], ['class' => 'btn btn-success btn-xs']) ?>
+            <?= Html::a('Delete', ['delete', 'id' => $todo->id], [
+                'class' => 'btn btn-danger btn-xs',
+                'data' => [
+                    'confirm' => 'このタスクを消去しますか?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
     </li>
+--------------------------------------------------------------------------------------------------------
 <?php endforeach; ?>
+
+<p>
+    
+</p>
+
+
+
+<!--
+
+HTTP metthod
+GET POST PUT DELETE
+HTML
+a link
+submit
+
+-->
