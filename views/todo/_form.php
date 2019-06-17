56ab563d70e9c6363353c24e25b3e03fd9e41812
川北 紘正 <kawakita@mediba.jp>
2019-06-17 11:16:28 +0900

    add category at _form.php

--- views/todo/_form.php
+++ views/todo/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Country */
@@ -12,12 +13,28 @@ use yii\widgets\ActiveForm;
 
 <?php $form = ActiveForm::begin(); ?>
 
-<?= $form->field($model, 'task')->textInput(['maxlength' => true])->hint("タスクを入力してください。(最大255文字)")->label("Task") ?>
+<?= $form->field($model, 'datetime')->textInput(['maxlength' => true])->hint("期限を記入してください 例：2019-06-03 12:30:00")->label("期限") ?>
 
-<!-- <?= $form->field($model, 'done')->textInput()->hint("達成：1, 未達成：0を入力してください")->label("Done") ?> -->
+
+<?= $form->field($model, 'task')->textInput(['maxlength' => true])->hint("タスクを入力してください。(最大255文字)")->label("タスク") ?>
+
+<?= $form->field($model, 'description')->textInput(['maxlength' => true])->hint("タスクの詳細を入力してください。(最大255文字)")->label("メモ") ?>
+
+<P>
+    <?= $form->field($model, 'category')->dropdownList([
+            "イベント" => 'イベント', 
+            "タスク" => 'タスク',
+            "アイディア" => 'アイディア',
+            "プラン" => 'プラン',
+            "イシュー" => 'イシュー'
+        ],
+        ['prompt'=>'カテゴリーを選択してください']
+    )->label("カテゴリー");
+    ?>
+</P>
 
 <div class="form-group">
-    <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    <?= Html::submitButton('保存', ['class' => 'btn btn-success']) ?>
 </div>
 
 <?php ActiveForm::end(); ?>
